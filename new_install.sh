b331e8db40b051f9534d0363e062b4ce9ceb68e2
lordsvein <lordsvein_tv@ploud.fr>
2022-05-08 20:30:00 +0200

    add chaotic aur

--- new_install.sh
+++ new_install.sh
@@ -59,6 +59,13 @@ do
 
     if [ "$distro" = "${arch[i]}"  ] ;
     then
+
+        ###install chaotic-aur
+        sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+        sudo pacman-key --lsign-key FBA220DFC880C036
+        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+        sudo sudo echo -e "\n[chaotic-aur] \nInclude = /etc/pacman.d/chaotic-mirrorlist " | sudo tee -a  /etc/pacman.conf
+
         ####Software Installer
         ##Natif
         sudo pacman -Syyuu --noconfirm  base-devel flatpak snapd keepassxc steam latte-dock nextcloud-client kvantum zenity
